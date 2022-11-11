1ef1b2720f41df332439ffb721a6238d45180214
Rodolfo García Peñas (kix) <kix@kix.es>
2022-11-11 12:08:42 +0100

    setup.py reformat file
    This patch only reformats the file.

--- setup.py
+++ setup.py
@@ -5,43 +5,44 @@ from setuptools import setup
 
 # Error-handling here is to allow package to be built w/o README included
 try:
-	readme = open(os.path.join(
-		os.path.dirname(__file__), 'README.txt' )).read()
-except IOError: readme = ''
+    readme = open(os.path.join(
+        os.path.dirname(__file__), 'README.txt')).read()
+except IOError:
+    readme = ''
 
 from nflog_cffi import NFLOG
 
 setup(
+    name = 'scapy-nflog-capture',
+    version = '22.11.1',
+    author = 'Rodolfo García Peñas (kix)',
+    author_email = 'kix@kix.es',
+    license = 'WTFPL',
+    keywords = ['nflog', 'scapy', 'plugin', 'driver', 'traffic', 'analysis',
+                'analyze', 'capture', 'dump', 'network', 'linux', 'security',
+                'sniffer', 'spoof', 'netfilter', 'iptables', 'xtables',
+                'filter', 'filtering', 'firewall', 'audit', 'cffi',
+                'libnetfilter_log', 'socket'],
+    url = 'http://github.com/mk-fg/scapy-nflog-capture',
 
-	name = 'scapy-nflog-capture',
-	version = '22.11.1',
-	author = 'Rodolfo García Peñas (kix)',
-	author_email = 'kix@kix.es',
-	license = 'WTFPL',
-	keywords = [ 'nflog', 'scapy', 'plugin', 'driver', 'traffic', 'analysis',
-		'analyze', 'capture', 'dump', 'network', 'linux', 'security', 'sniffer', 'spoof',
-		'netfilter', 'iptables', 'xtables', 'filter', 'filtering', 'firewall', 'audit',
-		'cffi', 'libnetfilter_log', 'socket' ],
-	url = 'http://github.com/mk-fg/scapy-nflog-capture',
+    description = 'Driver for scapy to allow capturing packets via Linux NFLOG interface',
+    long_description = readme,
 
-	description = 'Driver for scapy to allow capturing packets via Linux NFLOG interface',
-	long_description = readme,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: System :: Operating System Kernels :: Linux'],
 
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'Intended Audience :: System Administrators',
-		'Intended Audience :: Telecommunications Industry',
-		'License :: OSI Approved',
-		'Operating System :: POSIX :: Linux',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3 :: Only',
-		'Topic :: Security',
-		'Topic :: System :: Networking :: Monitoring',
-		'Topic :: System :: Operating System Kernels :: Linux' ],
+    ext_modules = [NFLOG().ffi.verifier.get_extension()],
 
-	ext_modules = [NFLOG().ffi.verifier.get_extension()],
-
-	py_modules = ['nflog_cffi', 'scapy_nflog'],
-	package_data = {'': ['README.txt']} )
+    py_modules = ['nflog_cffi', 'scapy_nflog'],
+    package_data = {'': ['README.txt']})
